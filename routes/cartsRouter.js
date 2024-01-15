b37a6b5fb4f9464750d1ad1f2e8b219bce064436
santiagoand0 <156715231+santiagoand0@users.noreply.github.com>
2024-01-15 18:18:28 -0300

    Add files via upload

--- routes/cartsRouter.js
+++ routes/cartsRouter.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
+const path = require('path');
+
+function leerCarritos() {
+    try {
+      let data = fs.readFileSync(path.join(__dirname, 'carritos.json'), 'utf8');
+      return JSON.parse(data);
+    } catch (error) {
+      return [];
+    }
+   }
 
   router.post('/', (req, res) => {
     let carritos = leerCarritos();
